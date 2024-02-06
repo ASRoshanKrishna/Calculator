a93797fe72eecce9d159b475d3df328e16d4bf65
Roshan Krishna A.S <asroshank@gmail.com>
2024-02-06 21:10:00 +0530

    func for first expression

--- myScript.js
+++ myScript.js
@@ -1,34 +1,48 @@
-let firstOperand = 6;
-let operator = "+";
-let secondOperand = 2;
-
-function add(firstOperand, secondOperand){
-    return firstOperand + secondOperand;
-}
-
-function subtract(firstOperand, secondOperand){
-    return firstOperand - secondOperand;
-}
-
-function multiply(firstOperand, secondOperand){
-    return firstOperand * secondOperand;
-}
-
-function divide(firstOperand, secondOperand){
-    return firstOperand / secondOperand;
-}
+let firstOperand;
+let operator;
+let secondOperand;
+let variable = 0;
 
 function operate(operator, firstOperand, secondOperand){
     switch(operator){
         case "+":
-            return add(firstOperand, secondOperand);
+             console.log(firstOperand + secondOperand);
+             break;
         case "-":
-            return subtract(firstOperand, secondOperand);
+            console.log(firstOperand - secondOperand);
+            break;
         case "*":
-            return multiply(firstOperand, secondOperand);
+            console.log(firstOperand * secondOperand);
+            break;
         case "/":
-            return divide(firstOperand, secondOperand);
+            console.log(firstOperand / secondOperand);
+            break;
     }
 }
 
-console.log(operate(operator, firstOperand, secondOperand));
+const display = document.querySelector(".display0");
+
+function dis(n){
+    if(typeof n === "number"){
+        display.textContent += n;
+        variable = variable*10 + n;
+        console.log(variable);
+    }
+    else{
+        if(!firstOperand){
+            display.textContent = "";
+            firstOperand = variable;
+            console.log("first is "+firstOperand);
+            variable = 0;
+            operator = n;
+        }
+        else{
+            display.textContent = "";
+            secondOperand = variable;
+            console.log("second is "+secondOperand);
+            variable = 0;
+            operate(operator, firstOperand,secondOperand);
+        }
+    }
+}
+
