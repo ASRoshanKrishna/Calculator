7398b72ce8c92bd11de5993184f1858ad9bc6ae6
Roshan Krishna A.S <asroshank@gmail.com>
2024-02-06 23:35:37 +0530

    complete calculation and display part

--- myScript.js
+++ myScript.js
@@ -1,21 +1,34 @@
-let firstOperand;
-let operator;
-let secondOperand;
+let firstOperand = null;
+let operator = null;
+let secondOperand = null;
 let variable = 0;
 
 function operate(operator, firstOperand, secondOperand){
+    console.log(operator, firstOperand,secondOperand);
     switch(operator){
         case "+":
-             console.log(firstOperand + secondOperand);
-             break;
+            firstOperand = firstOperand + secondOperand;
+            display.textContent = firstOperand;
+            console.log(firstOperand)
+            return firstOperand;
+            break;
         case "-":
-            console.log(firstOperand - secondOperand);
+            firstOperand = firstOperand - secondOperand;
+            display.textContent = firstOperand;
+            console.log(firstOperand)
+            return firstOperand;
             break;
         case "*":
-            console.log(firstOperand * secondOperand);
+            firstOperand = firstOperand * secondOperand;
+            display.textContent = firstOperand;
+            console.log(firstOperand)
+            return firstOperand;
             break;
         case "/":
-            console.log(firstOperand / secondOperand);
+            firstOperand = firstOperand / secondOperand;
+            display.textContent = firstOperand;
+            console.log(firstOperand)
+            return firstOperand;
             break;
     }
 }
@@ -24,25 +37,35 @@ const display = document.querySelector(".display0");
 
 function dis(n){
     if(typeof n === "number"){
-        display.textContent += n;
+        if(firstOperand != null) display.textContent = "";
+        console.log("if");
         variable = variable*10 + n;
+        display.textContent = variable;
         console.log(variable);
     }
     else{
-        if(!firstOperand){
-            display.textContent = "";
+        if(firstOperand == null){
             firstOperand = variable;
             console.log("first is "+firstOperand);
             variable = 0;
             operator = n;
+            display.textContent = "";
         }
+        else if(operator == null) operator = n;
         else{
             display.textContent = "";
             secondOperand = variable;
             console.log("second is "+secondOperand);
             variable = 0;
-            operate(operator, firstOperand,secondOperand);
+            let f = operate(operator, firstOperand,secondOperand);
+            firstOperand = f;
+            operator = n;
+            secondOperand = null;   
+        }
+        if(n === "="){
+            operator = null;
         }
     }
+    console.log(operator, firstOperand,secondOperand);
 }
 
