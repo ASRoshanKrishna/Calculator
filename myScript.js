68ddc821f4067a20454d59e5b3100ed0fd812cf7
Roshan Krishna A.S <asroshank@gmail.com>
2024-02-06 18:18:28 +0530

    create simple math funcs and index

---
+++ myScript.js
@@ -0,0 +1,20 @@
+function add(firstOperand, secondOperand){
+    return firstOperand + secondOperand;
+}
+
+function subtract(firstOperand, secondOperand){
+    return firstOperand - secondOperand;
+}
+
+function multiply(firstOperand, secondOperand){
+    return firstOperand * secondOperand;
+}
+
+function divide(firstOperand, secondOperand){
+    return firstOperand / secondOperand;
+}
+
+let a = 6;
+let b = 2;
+
+console.log(add(a,b), subtract(a,b), multiply(a,b), divide(a,b))
