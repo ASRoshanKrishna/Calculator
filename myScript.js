1acd41f2b9d90c015f18aca8bc02366589c9d3de
Roshan Krishna A.S <asroshank@gmail.com>
2024-02-23 19:58:05 +0530

    remove console.log

--- myScript.js
+++ myScript.js
@@ -15,7 +15,6 @@ function backspace(){
         disvariable = parseInt(disvariable/10);
         display.textContent = disvariable;
         variable = disvariable;
-        console.log("stop",operator, firstOperand, secondOperand, variable, deciCheck);
     }
 }
 
@@ -23,7 +22,6 @@ function opcolor(opn){
     for(let i=1;i<=4;i++){
         if(i!=opn){
             idname = '#op'+i;
-            console.log(idname);
             basicop = document.querySelector(idname);
             basicop.style.backgroundColor = "rgb(33, 43, 65)";
         }
@@ -34,7 +32,6 @@ function opcolor(opn){
 }
 
 function operate(operator, firstOperand, secondOperand){
-    console.log(operator, firstOperand,secondOperand);
     idname = '#op'+opn;
     basicop = document.querySelector(idname);
     basicop.style.backgroundColor = "rgb(33, 43, 65)"
@@ -44,27 +41,23 @@ function operate(operator, firstOperand, secondOperand){
             firstOperand = Math.round(firstOperand * 100) / 100;
             disvariable = firstOperand;
             display.textContent = parseFloat(disvariable);
-            console.log(firstOperand)
             return firstOperand;
         case "-":
             firstOperand = firstOperand - secondOperand;
             firstOperand = Math.round(firstOperand * 100) / 100;
             disvariable = firstOperand;
             display.textContent = parseFloat(disvariable);
-            console.log(firstOperand)
             return firstOperand;
         case "*":
             firstOperand = firstOperand * secondOperand;
             firstOperand = Math.round(firstOperand * 100) / 100;
             disvariable = firstOperand;
             display.textContent = parseFloat(disvariable);
-            console.log(firstOperand)
             return firstOperand;
         case "/":
             if(secondOperand == 0){
                 let joke = "You silly!";
                 display.textContent = joke;
-                console.log(joke);
                 firstOperand = null;
                 return firstOperand;
             }
@@ -72,7 +65,6 @@ function operate(operator, firstOperand, secondOperand){
             firstOperand = Math.round(firstOperand * 100) / 100;
             disvariable = firstOperand;
             display.textContent = parseFloat(disvariable);
-            console.log(firstOperand)
             return firstOperand;
     }
 }
@@ -92,10 +84,8 @@ function perc(){
 
 function dis(n){
     n1=n;
-    console.log("start",operator, firstOperand, secondOperand, variable, deciCheck, n);
     if(typeof n === "number" || n =='.'){
         if(firstOperand != null) display.textContent = "";
-        console.log("if");
         if(typeof n === "number" && deciCheck == 0)
         variable = variable*10 + n;
         else{
@@ -108,14 +98,12 @@ function dis(n){
         }
         disvariable = variable;
         display.textContent = disvariable;
-        console.log(variable);
     }
     else{
         if(n == '/') opn = 1;
         else if(n == '*') opn = 2;
         else if(n == '-') opn = 3;
         else if(n == '+') opn = 4;
-        console.log(opn);
         if(firstOperand && !operator){
             if(variable!=null)
             firstOperand = variable;
@@ -130,14 +118,12 @@ function dis(n){
         }
         else if(firstOperand == null){
             firstOperand = variable;
-            console.log("first is "+firstOperand);
             variable = 0;
             operator = n;
             opcolor(opn);
             deciCheck=0;
         }
         else if(operator == null){
-            console.log("empty");
             firstOperand = variable;
             variable = 0;
             operator = n;
@@ -147,7 +133,6 @@ function dis(n){
             disvariable = "";
             display.textContent = disvariable;
             secondOperand = variable;
-            console.log("second is "+secondOperand);
             variable = 0;
             f = operate(operator, parseFloat(firstOperand),parseFloat(secondOperand));
             firstOperand = f;
@@ -162,7 +147,6 @@ function dis(n){
             idname = '#op'+opn;
             basicop = document.querySelector(idname);
             basicop.style.backgroundColor = "rgb(33, 43, 65)";
-            console.log(operator, firstOperand,secondOperand);
             if(operator == "="){
                 operator = "+";
                 secondOperand = 0;
@@ -177,28 +161,21 @@ function dis(n){
             eq = 1;
         }
     }
-    if(deciCheck==1)
-    console.log("stop",operator, parseFloat(firstOperand),parseFloat(secondOperand), variable, deciCheck, n);
-    console.log("stop",operator, firstOperand, secondOperand, variable, deciCheck, n);
 }
 
 document.addEventListener('keydown', (event) => {
     let kbintvalue = parseInt(event.key);
     let kbvalue = event.key;
     if(kbintvalue >=0 && kbintvalue <=9 || kbintvalue == '.'){
-        console.log(typeof kbintvalue, kbintvalue);
         dis(kbintvalue);
     }
     else if(kbvalue == '/' || kbvalue == '-' || kbvalue == '*' || kbvalue == '+'){
-        console.log(typeof kbvalue, kbvalue);
         dis(kbvalue);
     }
     else if(kbvalue == "Backspace"){
-        console.log(typeof kbvalue, kbvalue);
         backspace();
     }
     else if(kbvalue == "Enter"){
-        console.log(typeof kbvalue, kbvalue);
         kbvalue = "=";
         dis(kbvalue);
     }
