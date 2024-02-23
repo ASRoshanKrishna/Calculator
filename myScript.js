7c74ac853a0a9822da909434631681dcfe6d4ca8
Roshan Krishna A.S <asroshank@gmail.com>
2024-02-23 19:47:35 +0530

    save with console.log

--- myScript.js
+++ myScript.js
@@ -3,19 +3,19 @@ let operator = null;
 let secondOperand = null;
 let variable = 0;
 let disvariable = 0;
-let d = 0;
-let e = 0;
+let deciCheck = 0;
+let eq = 0;
 let basicop = null;
 let idname;
 let opn;
-let z;
+let n1;
 
 function backspace(){
-    if(typeof z == "number"){
+    if(typeof n1 == "number"){
         disvariable = parseInt(disvariable/10);
         display.textContent = disvariable;
         variable = disvariable;
-        console.log("stop",operator, firstOperand, secondOperand, variable, d);
+        console.log("stop",operator, firstOperand, secondOperand, variable, deciCheck);
     }
 }
 
@@ -84,27 +84,27 @@ function perc(){
     else firstOperand = parseFloat(firstOperand) / 100;
     disvariable = firstOperand;
     display.textContent = disvariable;
-    if(firstOperand%1==0) d =0;
-    else d = 1;
+    if(firstOperand%1==0) deciCheck =0;
+    else deciCheck = 1;
     variable = null;
     operator = null;
 }
 
 function dis(n){
-    z=n;
-    console.log("start",operator, firstOperand, secondOperand, variable, d, n);
+    n1=n;
+    console.log("start",operator, firstOperand, secondOperand, variable, deciCheck, n);
     if(typeof n === "number" || n =='.'){
         if(firstOperand != null) display.textContent = "";
         console.log("if");
-        if(typeof n === "number" && d == 0)
+        if(typeof n === "number" && deciCheck == 0)
         variable = variable*10 + n;
         else{
-            if(n=="." && d==0 || n!="."){
+            if(n=="." && deciCheck==0 || n!="."){
                 if(variable == null) variable = "";
                 variable = variable.toString();
                 variable = variable + n;
             }
-            d = 1;
+            deciCheck = 1;
         }
         disvariable = variable;
         display.textContent = disvariable;
@@ -122,7 +122,7 @@ function dis(n){
             variable = 0;
             operator = n;
             opcolor(opn);
-            d=0;
+            deciCheck=0;
         }
         else if(firstOperand && operator && variable == 0 && typeof n != "number" && n != "="){
             operator = n;
@@ -134,7 +134,7 @@ function dis(n){
             variable = 0;
             operator = n;
             opcolor(opn);
-            d=0;
+            deciCheck=0;
         }
         else if(operator == null){
             console.log("empty");
@@ -151,8 +151,8 @@ function dis(n){
             variable = 0;
             f = operate(operator, parseFloat(firstOperand),parseFloat(secondOperand));
             firstOperand = f;
-            if(firstOperand%1 == 0) d = 0;
-            else d = 1;
+            if(firstOperand%1 == 0) deciCheck = 0;
+            else deciCheck = 1;
             if(n!="=")
             operator = n;
             opcolor(opn);
@@ -169,17 +169,17 @@ function dis(n){
                 let f = operate(operator, parseFloat(firstOperand),parseFloat(secondOperand));
             } 
             variable = f;
-            if(variable%1 == 0) d = 0;
-            else d = 1;
+            if(variable%1 == 0) deciCheck = 0;
+            else deciCheck = 1;
             firstOperand = variable;
             variable = null;
             operator = null;
-            e = 1;
+            eq = 1;
         }
     }
-    if(d==1)
-    console.log("stop",operator, parseFloat(firstOperand),parseFloat(secondOperand), variable, d, n);
-    console.log("stop",operator, firstOperand, secondOperand, variable, d, n);
+    if(deciCheck==1)
+    console.log("stop",operator, parseFloat(firstOperand),parseFloat(secondOperand), variable, deciCheck, n);
+    console.log("stop",operator, firstOperand, secondOperand, variable, deciCheck, n);
 }
 
 document.addEventListener('keydown', (event) => {
@@ -203,4 +203,5 @@ document.addEventListener('keydown', (event) => {
         dis(kbvalue);
     }
     else console.log(typeof kbvalue, kbvalue);
+    document.activeElement.blur()
 });
